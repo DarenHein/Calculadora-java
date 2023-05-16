a4c01a1b64b73b2deee61dab3cc75b4cfaf1b81f
DarenHein <darenhein@hotmail.com>
2023-05-16 02:20:35 +0200

    mamalona

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("hola mundo2");
         Ventana obj = new Ventana(); 
         obj.setVisible(true);
     }
